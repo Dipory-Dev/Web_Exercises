dbe71afa47f77dc8f48dca939f6fb486da5eec7b
kjs106203 <kjs106203@naver.com>
2024-10-16 15:25:38 +0900

    mybatis로 JDBC 교체

--- src/main/java/com/my/model/dao/MyBoardDao.java
+++ src/main/java/com/my/model/dao/MyBoardDao.java
@@ -1,22 +1,32 @@
 package com.my.model.dao;
 
 import com.my.model.dto.MyBoard;
+import org.apache.ibatis.session.SqlSession;
+
 
 import java.sql.*;
 import java.util.ArrayList;
 import java.util.List;
 
-public class MyBoardDao {
+public class MyBoardDao extends SqlMapConfig{
 
     public List<MyBoard> selectAll() {
         List<MyBoard> res = new ArrayList<>();
+        SqlSession session = null;
 
+        session = getSqlSessionFactory().openSession(true);
+        res = session.selectList("com.my.model.dao.MyBoardDao.selectAll");
 
+        session.close();
         return res;
     }
 
     public MyBoard selectOne(int myno) {
         MyBoard res = new MyBoard();
+        SqlSession session = null;
+
+        session = getSqlSessionFactory().openSession(true);
+        res = session.selectOne("com.my.model.dao.MyBoardDao.selectOne", myno);
 
 
         return res;
@@ -25,7 +35,9 @@ public class MyBoardDao {
 
     public int insert(MyBoard dto) {
         int res = 0;
-
+        SqlSession session = null;
+        session = getSqlSessionFactory().openSession(true);
+        res = session.insert("com.my.model.dao.MyBoardDao.insert", dto);
 
         return res;
     }
