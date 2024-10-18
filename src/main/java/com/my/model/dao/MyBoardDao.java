948d29717ef532bdec926f7aa13aed31810a6196
kjs106203 <kjs106203@naver.com>
2024-10-18 09:28:26 +0900

    mybatis 삽입 기능 추가

--- src/main/java/com/my/model/dao/MyBoardDao.java
+++ src/main/java/com/my/model/dao/MyBoardDao.java
@@ -2,6 +2,7 @@ package com.my.model.dao;
 
 import com.my.model.dto.MyBoard;
 import org.apache.ibatis.session.SqlSession;
+import org.apache.ibatis.session.SqlSessionFactory;
 
 
 import java.sql.*;
@@ -14,10 +15,17 @@ public class MyBoardDao extends SqlMapConfig{
         List<MyBoard> res = new ArrayList<>();
         SqlSession session = null;
 
-        session = getSqlSessionFactory().openSession(true);
-        res = session.selectList("com.my.model.dao.MyBoardDao.selectAll");
+        try {
+            session = getSqlSessionFactory().openSession(true);
+            res = session.selectList("com.my.model.dao.MyBoardDao.selectAll");
+        } catch (Exception e) {
+            e.printStackTrace();
+        } finally {
+            session.close();
+        }
 
-        session.close();
+
+
         return res;
     }
 
@@ -25,9 +33,14 @@ public class MyBoardDao extends SqlMapConfig{
         MyBoard res = new MyBoard();
         SqlSession session = null;
 
-        session = getSqlSessionFactory().openSession(true);
-        res = session.selectOne("com.my.model.dao.MyBoardDao.selectOne", myno);
-
+        try{
+            session = getSqlSessionFactory().openSession(true);
+            res = session.selectOne("com.my.model.dao.MyBoardDao.selectOne", myno);
+        } catch (Exception e) {
+            e.printStackTrace();
+        } finally {
+            session.close();
+        }
 
         return res;
     }
@@ -36,22 +49,14 @@ public class MyBoardDao extends SqlMapConfig{
     public int insert(MyBoard dto) {
         int res = 0;
         SqlSession session = null;
-        session = getSqlSessionFactory().openSession(true);
-        res = session.insert("com.my.model.dao.MyBoardDao.insert", dto);
-
-        return res;
-    }
-
-    public int update(MyBoard dto) {
-        int res = 0;
-
-
-        return res;
-    }
-
-    public int delete(int myno) {
-        int res = 0;
-
+        try {
+            session = getSqlSessionFactory().openSession(true);
+            res = session.insert("com.my.model.dao.MyBoardDao.insert", dto);
+        } catch (Exception e) {
+            e.printStackTrace();
+        } finally {
+            session.close();
+        }
 
         return res;
     }
