9073c27791cadd7352cd876580e32494654362d5
kjs106203 <kjs106203@naver.com>
2024-11-19 13:31:54 +0900

    JPA를 이용한 CRUD 작업 / application.properties에서 프로그램 실행시 db 생성을 update로 변경

--- SpringBoot04/src/main/java/com/boot/jpa/model/controller/JpaController.java
+++ SpringBoot04/src/main/java/com/boot/jpa/model/controller/JpaController.java
@@ -2,6 +2,7 @@ package com.boot.jpa.model.controller;
 
 import com.boot.jpa.model.dao.JpaDao;
 import com.boot.jpa.model.entity.JpaEntity;
+import oracle.jdbc.proxy.annotation.Post;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
@@ -35,6 +36,30 @@ public class JpaController {
     public String insert(JpaEntity dto) {
         dto.setMydate(new Date());
         dao.save(dto);
+        return "redirect:list";
+    }
+
+    @GetMapping("/detail")
+    public String detail(int myno, Model model) {
+        model.addAttribute("dto", dao.findByMyno(myno));
+        return "jpadetail";
+    }
+
+    @GetMapping("/updateform")
+    public String updateForm(int myno, Model model) {
+        model.addAttribute("dto", dao.findByMyno(myno));
+        return "jpaupdate";
+    }
+
+    @PostMapping("/update")
+    public String update(JpaEntity dto) {
+        dao.save(dto);
+        return "redirect:detail?myno=" + dto.getMyno();
+    }
+
+    @GetMapping("/delete")
+    public String delete(int myno) {
+        dao.deleteByMyno(myno);
         return "redirect:list";
     }
 }
--- SpringBoot04/src/main/java/com/boot/jpa/model/dao/JpaDao.java
+++ SpringBoot04/src/main/java/com/boot/jpa/model/dao/JpaDao.java
@@ -4,6 +4,7 @@ import com.boot.jpa.model.entity.JpaEntity;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import javax.transaction.Transactional;
 import java.util.List;
 
 @Repository
@@ -12,4 +13,10 @@ public interface JpaDao extends JpaRepository<JpaEntity, Integer> {
     public List<JpaEntity> findAll();
 
     public JpaEntity save(JpaEntity dao);
+
+    public JpaEntity findByMyno(int myno);
+
+    @Transactional
+    public void deleteByMyno(int myno);
+
 }
